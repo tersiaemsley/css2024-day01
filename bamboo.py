842102097f139f31e308a9d18aeb08f9ebdb3718
tersiaemsley <132990960+tersiaemsley@users.noreply.github.com>
2024-01-30 10:37:30 +0200

    Add files via upload

---
+++ bamboo.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 14:56:29 2024
+
+@author: Tersia Emily
+"""
+"""
+
+Dictinaries - key:value pairs
+
+cats "a cute animal"
+
+
+"""
+
+mammals = {"cat":"a cute animal","lion":"king of the jungle","elephant":"a gigantic herbivore"}
+
+
+print(mammals["cat"])
+
+
+"""
+data frames
+"""
+
+fuits = ["apple","banana", "orange", "orange", "grape"]
+ 
+size_nm = [9, 8, 10.1, 13.2, 20.5]
+
+prices = [10.00, 12.50, 16.00, 12.50, 16.00, 23.00]
+
+df['prices'] = prices
+
+"""
+
+file = panada.read_csv("ountry_data.csv")
